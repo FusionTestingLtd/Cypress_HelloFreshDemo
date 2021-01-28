0383e3523e2eacb024866039afba4e98a9e9607e
Ozzy <FusionTestingLtd@users.noreply.github.com>
2021-01-28 13:59:26 +0000

    added some navigation tests

--- cypress/integration/MyTests/Shared/navigation.spec.js
+++ cypress/integration/MyTests/Shared/navigation.spec.js
@@ -16,47 +16,49 @@ describe("Navigation Test", function () {
     cy.get('[data-test-id="desktop-hellofresh-logo"]').should(
       "have.attr",
       "alt",
-      "HelloFresh"
-    ); //<-----
+      this.data.brandLogoAlt
+    );
     cy.get('[data-test-id="logo"]').should(
       "have.attr",
       "href",
-      "/?locale=en-GB"
-    ); //<---
+      this.data.brandLogoUrl
+    );
   });
 
   it("Navigation Bar: How it Works", function () {
     cy.get(
       '#main-navigation[role="navigation"] span a#link_our-recipe-boxes_en-GB'
     )
-      .should("have.text", "Our Recipe Boxes") //<---
-      .and("have.attr", "href", "/plans"); //<----
+      .should("have.text", this.data.recipieBoxText)
+      .and("have.attr", "href", this.data.recipieBoxUrl);
+      cy.wait(500)
 
     cy.get("span#links-left-navigation-public").within(function () {
       cy.get("span.fela-_d1ib85")
         .eq(0)
-        .should("contain", "How It Works") //<----
+        .should("contain", this.data.HowItWorksHeadingTitleText)
         .trigger("mouseover")
         .within(function () {
-          //<---
-          cy.get("a").eq(0).should("have.attr", "href", "/how-it-works/");
+          cy.get("a")
+            .eq(0)
+            .should("have.attr", "href", this.data.HowItWorksHeadingUrl);
           cy.get("span")
             .eq(1)
-            .should("have.text", "How It Works")
+            .should("have.text", this.data.HowItWorksHeadingLinkText)
             .and("be.visible");
           cy.get("a")
             .eq(1)
-            .should("have.attr", "href", "/about/sustainability");
+            .should("have.attr", "href", this.data.SustainabilityUrl);
           cy.get("span")
             .eq(2)
-            .should("have.text", "Sustainability")
+            .should("have.text", this.data.SustainabilityText)
             .and("be.visible");
           cy.get("a")
             .eq(2)
-            .should("have.attr", "href", "/about/carbon-neutral");
+            .should("have.attr", "href", this.data.C02NeutralUrl);
           cy.get("span")
             .eq(3)
-            .should("have.text", "C02 Neutral")
+            .should("have.text", this.data.C02NeutralText)
             .and("be.visible");
         });
       cy.get("span.fela-_d1ib85").eq(0).trigger("mouseout");
@@ -67,27 +69,39 @@ describe("Navigation Test", function () {
     cy.get("span#links-left-navigation-public").within(function () {
       cy.get("span.fela-_d1ib85")
         .eq(1)
-        .should("contain", "Our Recipes") //<----
+        .should("contain", this.data.OurRecipesTitleText)
         .trigger("mouseover")
         .within(function () {
-          //<---
-          cy.get("a").eq(0).should("have.attr", "href", "/menus/");
+          cy.get("a").eq(0).should("have.attr", "href", this.data.menuUrl);
           cy.get("span")
             .eq(1)
-            .should("have.text", "On The Menu")
+            .should("have.text", this.data.menuLinkText)
             .and("be.visible");
-          cy.get("a").eq(1).should("have.attr", "href", "/recipes/");
+          cy.get("a").eq(1).should("have.attr", "href", this.data.recipieUrl);
           cy.get("span")
             .eq(2)
-            .should("have.text", "Recipe Hub")
+            .should("have.text", this.data.recipieLinkText)
             .and("be.visible");
         });
+      cy.get("span.fela-_d1ib85").eq(1).trigger("mouseout");
     });
   });
 
-  it("Gift Cards", function () {});
+  it("COVID-19 Update", function () {
+    cy.get('span#links-left-navigation-public a[title="COVID-19 Updates"]')
+      .should("have.text", this.data.COVIDText)
+      .and("have.attr", "href", this.data.COVIDUrl);
+  });
 
-  it("COVID-19 Update", function () {});
+  it("Gift Cards", function () {
+    cy.get('span#links-left-navigation-public a[title="Gift Cards"]')
+      .should("have.text", this.data.GiftCardsText)
+      .and("have.attr", "href", this.data.GiftCardsUrl);
+  });
 
-  it("Log In Button", function () {});
+  it("Log In Button", function () {
+    cy.get('[data-test-id="menu-item-login-button"]')
+      .should("have.attr", "href", this.data.LoginUrl)
+      .and("have.text", this.data.LoginText);
+  });
 });
